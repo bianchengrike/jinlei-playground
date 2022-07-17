ca1258617d2aae911281a9cc438062a03f82c530
jinlei@daily_coding <zhang_jinlei@126.com>
2022-07-17 08:33:40 +0800

    day032 coding

---
+++ 2022/day-032.py
@@ -0,0 +1,6 @@
+#  ValueError: Input contains NaN, infinity or a value too large for dtype('float64').
+# 原因是在从数据库导入的数据中，格式不正确，2344566789493828193949 是int 类型，在用 pandas 读取进来的时候
+# 变成了float，导致上面的 ValueError,目前的一个解决思路是类似下面的办法
+
+df['int'] = str(df['int'])
+
